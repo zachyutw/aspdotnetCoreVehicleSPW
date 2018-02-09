a573180a37527e09d1b7dcfb1b318c7c1ca6444a
Zach Yu <zachyu.tw@gmail.com>
2018-02-08 19:48:59 -0800

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace askdotnetblog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new Exception();
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            return View();
+        }
+    }
+}
